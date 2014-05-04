e9ab4458ba4d9fb6e95748357edb47e8954d942a
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-05-04 11:36:10 -0300

    added pouchdb support

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+	createDatabase();
 
 $("#formulario").submit(function(event ){
 	event.preventDefault();
@@ -16,6 +16,8 @@ $("#formulario").submit(function(event ){
 	
 });
 
+showInfoMsg("Todo Listo! ingresa tu id ");
+
 }) ;
 
 
@@ -31,6 +33,13 @@ $("#formulario").submit(function(event ){
 
   function getWTItems(){	
   	console.log("entra aca");
+  	var array = new Array();
+  	db.allDocs({
+      include_docs: true, descending:true}, function (err,doc){
+        
+        reloadwts(doc.rows);
+    });
+ /* 
   			var array = new Array();
 	var wTime = database.transaction("workingtime").objectStore("workingtime");
 
@@ -59,9 +68,74 @@ wTime.openCursor().onerror = function (event){
 	alert("error en getusuarios!!");
 }
 
-
+*/
 
 };
+
+function isACheckin(employee){
+	var flag = true;
+	db.allDocs({
+      include_docs: true, descending:true}, function (err,docs){
+        for (var i = 0; i < docs.rows.length; i++) {
+       		if(docs.rows[i].doc.employee == employee ){
+       			console.log("entra al if 1");
+       			if(docs.rows[i].doc.checkout === null){
+       				console.log("entra al if 2");
+       				    	console.log("hace checkout");
+
+       				doCheckout(employee);
+       				return;
+       			}
+       		}
+
+        };
+
+    	console.log("hace checkin");
+    	doCheckin(employee);
+       
+    });
+    
+}
+
+function performCheckin(img,employee){
+	var wt = new WorkingTime();
+	wt.employee = employee;
+	wt.captcheckin = img;
+	wt.checkin = new Date();
+	db.post(wt, function(err, response){
+		if(err){
+			console.log("error al checkin! "+err);
+		}else{
+		
+			getWTItems();
+		}
+	})
+}
+
+function performCheckout(img,employee){
+db.allDocs({
+      include_docs: true, descending:true}, function (err,docs){
+      	if(err){
+      		console.log("error al performCheckout! "+err);
+      	}else{
+	        for (var i = 0; i < docs.rows.length; i++) {
+       		if(docs.rows[i].doc.employee == employee ){
+       			docs.rows[i].doc.captcheckout = img;
+       			docs.rows[i].doc.checkout = new Date();
+       			console.log("valor del doc "+docs.rows[i].doc);
+       			db.put(docs.rows[i].doc, function(errUpdate, respUpdate){
+       				if(errUpdate){
+       					console.log("error en update!! "+errUpdate);
+       				}else{
+       					getWTItems();      		
+       				}
+       			});
+       		}
+        };
+      	};
+    });
+
+}
   
 
 var reloadwts = function(wts){
@@ -71,24 +145,23 @@ var reloadwts = function(wts){
 	if (!wts){
 	return;
 	}
-
 	for(var i=0;i < wts.length; i++){
 		var row = "<tr>";
 
-    var imgCheckin = wts[i].captcheckin;
-	var imgCheckout = wts[i].captcheckout;
+    var imgCheckin = wts[i].doc.captcheckin;
+	var imgCheckout = wts[i].doc.captcheckout;
     var imgChin = "<img src='"+imgCheckin+"' width='100' height='100' class='snapshot' >";
     var imgChout = "";
-    if(wts[i].captcheckout != null){
+    if(wts[i].doc.captcheckout != null){
     	imgChout="<img src='"+imgCheckout+"' width='100' height='100' class='snapshot' >";
     }
     row = row +
-		"<td id='idwt'>"+wts[i].idwt+"</td>"+
-		"<td>"+wts[i].employee+"</td>"+
+		"<td id='idwt'>"+(i+1)+"</td>"+
+		"<td>"+wts[i].doc.employee+"</td>"+
 		"<td>"+imgChin+"</td>"+
-		"<td>"+wts[i].checkin+"</td>"+
+		"<td>"+wts[i].doc.checkin+"</td>"+
 		"<td>"+imgChout+"</td>"+
-		"<td>"+wts[i].checkout+"</td>"+
+		"<td>"+wts[i].doc.checkout+"</td>"+
 		"<td>"+"<button>x</button></td>"+
 		"</tr>";
 		$("#lista").append(row);
@@ -124,4 +197,39 @@ function deleteWT(wt){
 
   }
 
+function showErrMsg(msg){
 
+		$("#info").text(msg);
+		$("#info").removeClass("alert-info");
+		$("#info").removeClass("alert-success");
+		$("#info").addClass("alert-danger");
+		$("#info").fadeIn("fast");
+	setTimeout(function(){
+		$("#info").fadeOut("slow");
+	},5000);
+}
+
+function showSuccessMsg(msg){
+
+			$("#info").text(msg);
+		$("#info").removeClass("alert-info");
+		$("#info").removeClass("alert-danger");
+		$("#info").addClass("alert-success");
+		$("#info").fadeIn("fast");
+	setTimeout(function(){
+		$("#info").fadeOut("slow");
+	},5000);
+}
+
+function showInfoMsg(msg){
+	console.log("entra aca");
+			$("#info").val(msg);
+		$("#info").removeClass("alert-success");
+		$("#info").removeClass("alert-danger");
+		$("#info").addClass("alert-info");
+		$("#info").fadeIn("fast");
+	setTimeout(function(){
+		$("#info").fadeOut("slow");
+	},5000);
+
+}
---
+++ pouchtest.js
@@ -0,0 +1,131 @@
+
+
+
+  'use strict';
+
+  var ENTER_KEY = 13;
+
+  // EDITING STARTS HERE (you dont need to edit anything above this line)
+var db ;
+  var remoteCouch ;
+function createDatabase(){
+   db = new PouchDB("workingtime",{cache : false});
+    remoteCouch = 'http://192.168.1.200:5984/workingtime';
+
+  db.info(function(err,info){
+    db.changes({
+      since: info.update_seq,
+      live: true
+    }).on("change",getWTItems());
+    if (remoteCouch) {
+    sync();
+  } 
+  });
+   
+}
+
+
+function WorkingTime(){
+  this.employee = 0;
+  this.captcheckin = new Blob();
+  this.checkin = new Date();
+  this.captcheckout = null;
+  this.checkout = null;
+}
+
+
+  // We have to create a new todo document and enter it in the database
+  /*
+  function addTodo(text) {
+    var todo = {
+        _id: new Date().toISOString(),
+        title: text,
+        completed: false
+    };
+    db.put(todo, function callback(err,result){
+      if(err){
+        console.log("Exito! todo cargado");
+      }
+    });
+
+  }
+
+  // Show the current list of todos by reading them from the database
+  function showTodos() {
+    db.allDocs({
+      include_docs: true, descending:true}, function (err,doc){
+        //redrawTodosUI(doc.rows);
+    });
+  }
+*/
+/*
+  function checkboxChanged(todo, event) {
+    todo.completed = event.target.checked;
+    db.put(todo);
+  }
+*/
+  // User pressed the delete button for a todo, delete it
+  /*
+  function deleteButtonPressed(todo) {
+    db.remove(todo);
+  }
+*/
+  // The input box when editing a todo has blurred, we should save
+  // the new title or delete the todo if the title is empty
+  /*
+  function todoBlurred(todo, event) {
+    var trimmedText = event.target.value.trim();
+    if(!trimmedText){
+      db.remove(todo);
+    }else{
+      todo.title = trimmedText;
+      db.put(todo);
+    }
+  }
+*/
+  // Initialise a sync with the remote server
+  function sync() {
+   showInfoMsg("syncing");
+    var opts = {live: true};
+    db.replicate.to(remoteCouch,opts,showErrMsg);
+    db.replicate.from(remoteCouch,opts,showErrMsg);
+  }
+
+
+  // EDITING STARTS HERE (you dont need to edit anything below this line)
+
+  // There was some form or error syncing
+/*
+  function syncError() {
+    syncDom.setAttribute('data-sync-state', 'error');
+  }
+*/
+  // User has double clicked a todo, display an input so they can edit the title
+  /*
+  function todoDblClicked(todo) {
+    var div = document.getElementById('li_' + todo._id);
+    var inputEditTodo = document.getElementById('input_' + todo._id);
+    div.className = 'editing';
+    inputEditTodo.focus();
+  }
+
+  // If they press enter while editing an entry, blur it to trigger save
+  // (or delete)
+  function todoKeyPressed(todo, event) {
+    if (event.keyCode === ENTER_KEY) {
+      var inputEditTodo = document.getElementById('input_' + todo._id);
+      inputEditTodo.blur();
+    }
+  }
+*/
+  // Given an object representing a todo, this will create a list item
+  // to display it.
+
+/*
+  addEventListeners();
+  showTodos();
+
+  if (remoteCouch) {
+    sync();
+  }
+*/
