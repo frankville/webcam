b7ae3a4622efb93346e56dd9c3f5806bde7ccbed
Villasanti Francisco <villasanti.francisco@gmail.com>
2014-05-04 23:50:54 -0300

    changed some things

--- pouchtest.js
+++ pouchtest.js
@@ -10,7 +10,7 @@ var db ;
   var remoteCouch ;
 function createDatabase(){
    db = new PouchDB("workingtime");
-    remoteCouch = 'http://li707-122.members.linode.com:5984/workingtime';
+    remoteCouch = 'http://linode.dnsdynamic.com:5984/workingtime';
 
   db.info(function(err,info){
     db.changes({
